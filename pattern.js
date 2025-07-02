03022d63e388790bba9da91776b5900e16d251d0
mitchell <mitchell.gaertner@gmail.com>
2025-07-02 11:25:23 -0600

    added color lock functionality

--- pattern.js
+++ pattern.js
@@ -1,6 +1,6 @@
 var patternMaker = (typeof patternMaker == "undefined" || !patternMaker) ? {} : patternmaker;
 
-patternMaker.makeTopRows = (quantity, length) => {
+patternMaker.makeTopRows = async (quantity, length) => {
     let pattern = "";
 
     for(let i = 0; i < quantity; i++){
@@ -9,7 +9,7 @@ patternMaker.makeTopRows = (quantity, length) => {
             pattern += `<td class="bufferBlock"></td>`
         } 
         for (let j = 1; j <= length; j++){
-            pattern += `<td class="patternBlock" id="top-td-${i}-${j}"><input class="beadColor" type="color" value="#ffffff"></td>`
+            pattern += `<td class="patternBlock" id="top-td-${i}-${j}"><button class="colorBtn"><input class="beadColor vHid" type="color" value="#ffffff"></button></td>`
         }
         pattern += `</tr></table>`
     }
@@ -18,13 +18,13 @@ patternMaker.makeTopRows = (quantity, length) => {
 
 }
 
-patternMaker.makeBottomRows = (quantity, length) => {
+patternMaker.makeBottomRows = async (quantity, length) => {
     let pattern = "";
 
     for(let i = 0; i < quantity; i++){
         pattern += `<table id="bot-${i}"><tr>`
         for (let j = 0; j <= length; j++){
-            pattern += `<td class="patternBlock" id="bot-td-${i}-${j}"><input class="beadColor" type="color" value="#ffffff"></td>`
+            pattern += `<td class="patternBlock" id="bot-td-${i}-${j}"><button class="colorBtn"><input class="beadColor vHid" type="color" value="#ffffff"></button></td>`
         }
         pattern += `</tr></table>`
     }
@@ -52,7 +52,7 @@ patternMaker.getPatternJSON = () => {
         if (!out[half]["row" + row]){
             out[half]["row" + row] = []
         }
-        out[half]["row" + row].push(node.childNodes[0].value);
+        out[half]["row" + row].push(node.firstChild.firstChild.value);
 
     }
     return out;
@@ -77,14 +77,33 @@ patternMaker.export = (filename) => {
 patternMaker.import = async () => {
     let pattern = await document.querySelector("#upload").files[0].text()
     pattern = JSON.parse(pattern);
-    patternMaker.makeTopRows(Object.keys(pattern.top).length, pattern.top.row0.length);
-    patternMaker.makeBottomRows(Object.keys(pattern.bot).length, pattern.bot.row0.length - 1);
+    await patternMaker.makeTopRows(Object.keys(pattern.top).length, pattern.top.row0.length);
+    await patternMaker.makeBottomRows(Object.keys(pattern.bot).length, pattern.bot.row0.length - 1);
+    
+    document.querySelectorAll('.colorBtn').forEach(i => {
+        i.addEventListener('click', (e) => {
+            let locked = document.querySelector('#colorLockBool').checked
+            if (!locked){
+                e.target.querySelector('*').showPicker();
+            } else {
+                e.target.querySelector('*').value = document.querySelector('#colorLock').value
+                let newColor = e.target.querySelector('*').value;
+                e.target.style = "background-color: " + newColor;
+            }
+            
+        })
+        i.querySelector('*').addEventListener('change', (e) => {
+            i.style = 'background-color:' + e.target.value;
+        })
+    })
+
     let topKeys = Object.keys(pattern.top)
     for (let key of topKeys){
         let row = pattern.top[key];
         for(let i = 0; i<row.length; i++){
             let selector = "#top-td-" + key.replace(/\D/g, '') + "-" + (i + 1);
-            document.querySelector(selector).childNodes[0].value = row[i];
+            document.querySelector(selector).firstChild.firstChild.value = row[i];
+            document.querySelector(selector).firstChild.style = 'background-color:' + row[i];
         }
     }
     let botKeys = Object.keys(pattern.bot);
@@ -93,9 +112,8 @@ patternMaker.import = async () => {
         for(let i = 0; i < row.length; i++){
             let selector = "#bot-td-" + key.replace(/\D/g, '') + "-" + (i);
             let el = document.querySelector(selector);
-            if (el && el.childNodes){
-                el.childNodes[0].value = row[i];
-            }
+            el.firstChild.firstChild.value = row[i];
+            el.firstChild.style = 'background-color:' + row[i];
         }
     }
 }
